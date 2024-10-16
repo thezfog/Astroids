5aeba217ee8e771868b0965522cb866ba8c1406e
thezfog <thezan_@hotmail.com>
2024-10-16 12:50:31 +0200

    Added movement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from player import *
+from circleshape import *
 from constants import *
 
 
@@ -12,13 +14,20 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        pygame.display.update()
         dt = clock.tick(60)/1000
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.update()
     
 
 if __name__ == "__main__":
