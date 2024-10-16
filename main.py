91cf6edbea4a1ea411666c146a2061fcdfafac8b
thezfog <thezan_@hotmail.com>
2024-10-16 10:37:57 +0200

    First commit of Astroids

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+from constants import *
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(000, rect=None, special_flags=0)
+        pygame.display.update()
+    
+
+if __name__ == "__main__":
+    main()
+
